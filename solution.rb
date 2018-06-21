e873b07bf067f9ad7db149cb87b32de39e8a1c94
stivendiaz <stivendiazagudelo@gmail.com>
2018-06-21 09:44:48 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,8 +1,14 @@
 require "sinatra"
 
 
-get '/:nombre' do
+get '/' do
+    if(params[:nombre])
     <<-HTML
-    <h1> Hola #{params[:nombre]}! </h1>
+    <h1>Hola #{params[:nombre]}!</h1>
     HTML
+    else
+        <<-HTML
+    <h1>Hola desconocido!</h1>
+    HTML
+    end
 end
